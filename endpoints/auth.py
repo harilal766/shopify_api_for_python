87fd02f4b3787992755c06ef4ed98b53b647c037
harilal766 <harilalsunil2@gmail.com>
2025-06-23 12:52:22 +0530

    tested api creds

--- endpoints/auth.py
+++ endpoints/auth.py
@@ -5,7 +5,7 @@ with open('api_credentials.json','r') as json_file:
     api_creds = json.load(json_file)
     
 credential = tuple(api_creds.keys())[0]    
-    
+credential = api_creds[credential]
 
 ACCESS_TOKEN = credential["access_token"]
 STORENAME = credential["storename"]
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-
-
-
-
+from endpoints.auth import credential, STORENAME, API_KEY, API_SECRET, ACCESS_TOKEN
 
 
 class Test_Auth:
-    def test_credentials():
-        pass
+    def test_credentials(self):
+        assert credential
+        assert STORENAME
+        assert API_KEY
+        assert API_SECRET
+        assert ACCESS_TOKEN
+        
+        
